63a78bb2f27dc99e4b27650976373efad7c39ac9
Tarek Jassine <tarekjassine@gmail.com>
2024-07-08 23:42:06 +0200

    changed img and switched blocks

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -4,22 +4,6 @@ import Image4 from "../../../public/assets/img/portfolio/fleet-guard/1.png";
 
 const PortfolioData = [
   {
-    id: 2,
-    type: "Quellenchecker",
-    image: Image2,
-    tag: [""],
-    delayAnimation: "0",
-    modalDetails: [
-      {
-        project: "Web App",
-        client: "HAW Hamburg UNI",
-        language: "React.js, Node.js, Express.js, TypeScript, MongoDB",
-        preview: "Bildung",
-        link: "",
-      },
-    ],
-  },
-  {
     id: 3,
     type: "nu-dx:cycler",
     image: Image3,
@@ -31,6 +15,22 @@ const PortfolioData = [
         client: "nu:healthcare GmbH",
         language: "React.js, Electron.js, TypeScript, Node.js",
         preview: "Forschung",
+        link: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: "Quellenchecker",
+    image: Image2,
+    tag: [""],
+    delayAnimation: "0",
+    modalDetails: [
+      {
+        project: "Web App",
+        client: "HAW Hamburg UNI",
+        language: "React.js, Node.js, Express.js, TypeScript, MongoDB",
+        preview: "Bildung",
         link: "",
       },
     ],
