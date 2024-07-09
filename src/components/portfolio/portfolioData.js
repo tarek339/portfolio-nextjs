968ad174803b0beb433af451a9aa066227556b03
Tarek Jassine <tarekjassine@gmail.com>
2024-07-09 02:10:45 +0200

    updated portfolio

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -4,27 +4,11 @@ import Image4 from "../../../public/assets/img/portfolio/fleet-guard/1.png";
 
 const PortfolioData = [
   {
-    id: 2,
-    type: "Quellenchecker",
-    image: Image2,
-    tag: [""],
-    delayAnimation: "0",
-    modalDetails: [
-      {
-        project: "Web App",
-        client: "HAW Hamburg UNI",
-        language: "React.js, Node.js, Express.js, TypeScript, MongoDB",
-        preview: "Bildung",
-        link: "",
-      },
-    ],
-  },
-  {
     id: 3,
     type: "nu-dx:cycler",
     image: Image3,
     tag: [],
-    delayAnimation: "100",
+    delayAnimation: "0",
     modalDetails: [
       {
         project: "Desktop App",
@@ -41,13 +25,29 @@ const PortfolioData = [
     type: "fleet guard",
     image: Image4,
     tag: [""],
-    delayAnimation: "200",
+    delayAnimation: "100",
     modalDetails: [
       {
         project: "Web App",
         client: "Hamburg Verkehrsleitung",
         language: "React.js, Node.js, Express.js, TypeScript, MongoDB",
         preview: "Logistik",
+        link: "",
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: "Quellenchecker",
+    image: Image2,
+    tag: [""],
+    delayAnimation: "200",
+    modalDetails: [
+      {
+        project: "Web App",
+        client: "HAW Hamburg UNI",
+        language: "React.js, Node.js, Express.js, TypeScript, MongoDB",
+        preview: "Bildung",
         link: "",
       },
     ],
